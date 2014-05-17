4dce6997181502643c6d2f25970a576107c64909
Heiru Wu <wuheiru.5203@gmail.com>
2014-05-17 18:45:45 +0800

    First vresion completed

--- src/RCServer/RCServer.java
+++ src/RCServer/RCServer.java
@@ -11,6 +11,10 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class RCServer {
+	static int i;
+	static ServerSocket serverSocket;
+	static InputStream in;
+	static Socket client;
 	private static int key;
 
 	// private static ObjectInputStream fromClient;
@@ -23,73 +27,81 @@ public class RCServer {
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				final int ServerPort = 1025;
-				try {
-					System.out.println("Server: Waiting for connection......");
-					ServerSocket serverSocket = new ServerSocket(ServerPort);
-					Robot robot = new Robot();
-					while (true) {
-						Socket client = serverSocket.accept();
-						System.out.println("Server: Client connected");
-						// fromClient = new
-						// ObjectInputStream(client.getInputStream());
-						// fromServer = new
-						// ObjectOutputStream(client.getOutputStream());
-						InputStream in = client.getInputStream();
-						int i;
-						do {
-							i = in.read();
-							// ChoicesServer choice =
-							// (ChoicesServer)fromClient.readObject();
-							// System.out.println("the flag is " +
-							// choice.getKey());
-							// key = choice.getKey();
-							switch (i) {
-							case 0:
-								robot.keyPress(KeyEvent.VK_SHIFT);
-								Thread.sleep(20);
-								robot.keyPress(KeyEvent.VK_F5);
-								Thread.sleep(10);
-								robot.keyRelease(KeyEvent.VK_F5);
-								robot.keyRelease(KeyEvent.VK_SHIFT);
-								Thread.sleep(10);
-								break;
+				int ServerPort = 1025;
+				System.out.println("Server: Waiting for connection......");
+				while (true) {
+					try {
+						serverSocket = new ServerSocket(ServerPort);
+						Robot robot = new Robot();
+						while (true) {
+							client = serverSocket.accept();
+							// fromClient = new
+							// ObjectInputStream(client.getInputStream());
+							// fromServer = new
+							// ObjectOutputStream(client.getOutputStream());
+							in = client.getInputStream();
+							do {
+								i = in.read();
+								if (i == 5) {
+									client.close();
+									serverSocket.close();
+									System.out
+											.println("Server: Client connected");
+									break;
+								}
+								// ChoicesServer choice =
+								// (ChoicesServer)fromClient.readObject();
+								// System.out.println("the flag is " +
+								// choice.getKey());
+								// key = choice.getKey();
+								switch (i) {
+								case 0:
+									robot.keyPress(KeyEvent.VK_SHIFT);
+									Thread.sleep(20);
+									robot.keyPress(KeyEvent.VK_F5);
+									Thread.sleep(10);
+									robot.keyRelease(KeyEvent.VK_F5);
+									robot.keyRelease(KeyEvent.VK_SHIFT);
+									Thread.sleep(10);
+									break;
 
-							case 1:
-								robot.keyPress(KeyEvent.VK_LEFT);
-								Thread.sleep(10);
-								robot.keyRelease(KeyEvent.VK_LEFT);
-								Thread.sleep(10);
-								break;
+								case 1:
+									robot.keyPress(KeyEvent.VK_LEFT);
+									Thread.sleep(10);
+									robot.keyRelease(KeyEvent.VK_LEFT);
+									Thread.sleep(10);
+									break;
 
-							case 2:
-								robot.keyPress(KeyEvent.VK_RIGHT);
-								Thread.sleep(10);
-								robot.keyRelease(KeyEvent.VK_RIGHT);
-								Thread.sleep(10);
-								break;
+								case 2:
+									robot.keyPress(KeyEvent.VK_RIGHT);
+									Thread.sleep(10);
+									robot.keyRelease(KeyEvent.VK_RIGHT);
+									Thread.sleep(10);
+									break;
 
-							case 3:
-								robot.keyPress(KeyEvent.VK_ESCAPE);
-								Thread.sleep(10);
-								robot.keyPress(KeyEvent.VK_ESCAPE);
-								Thread.sleep(10);
-								break;
-							case 4:
-								System.out.println("Application Closed");
-								in.close();
-								client.close();
-								serverSocket.close();
-								break;
-							default:
-								break;
-							}
-						} while (i != 4);
+								case 3:
+									robot.keyPress(KeyEvent.VK_ESCAPE);
+									Thread.sleep(10);
+									robot.keyPress(KeyEvent.VK_ESCAPE);
+									Thread.sleep(10);
+									break;
+								case 4:
+									System.out
+											.println("Server: Client disconnected, waiting for another......");
+									in.close();
+									client.close();
+									serverSocket.close();
+									break;
+								default:
+									break;
+								}
+							} while (i != 4);
 							break;
+						}
+					} catch (Exception e) {
+						System.out.println("Server: Error");
+						e.printStackTrace();
 					}
-				} catch (Exception e) {
-					System.out.println("Server: Error");
-					e.printStackTrace();
 				}
 			}
 		});
