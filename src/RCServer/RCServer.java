d57535abe26a7d117bbd1041eb6f95aba438c297
Heiru Wu <wuheiru.5203@gmail.com>
2014-05-28 03:22:41 +0800

    Add mouse event

--- src/RCServer/RCServer.java
+++ src/RCServer/RCServer.java
@@ -1,18 +1,28 @@
 package RCServer;
 
 import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.rmi.server.SocketSecurityException;
 
 public class RCServer {
-	static int i;
+	static int i, j;
 	static ServerSocket serverSocket;
 	static InputStream in;
+	static ObjectInputStream ois;
 	static Socket client;
+	static int[] coordinate;
+//	static Point mouse;
 
 	public static void main(String[] args) throws IOException,
 			ClassNotFoundException, AWTException {
@@ -23,13 +33,17 @@ public class RCServer {
 				// TODO Auto-generated method stub
 				int ServerPort = 1025;
 				System.out.println("Server: Waiting for connection......");
-				while (true) {
-					try {
-						serverSocket = new ServerSocket(ServerPort);
-						Robot robot = new Robot();
-						while (true) {
-							client = serverSocket.accept();
-							in = client.getInputStream();
+				try {
+					serverSocket = new ServerSocket(ServerPort);
+					Robot robot = new Robot();
+					while (true) {
+						client = serverSocket.accept();
+						in = client.getInputStream();
+						j = in.read();
+						Thread.sleep(500);
+						if (j == 6) {
+							System.out
+									.println("Server: PowerPointControl Mode");
 							do {
 								i = in.read();
 								if (i == 5) {
@@ -81,15 +95,26 @@ public class RCServer {
 									break;
 								}
 							} while (i != 4);
-							break;
 						}
-					} catch (Exception e) {
-						System.out.println("Server: Error");
-						e.printStackTrace();
+						if (j == 7) {
+							System.out.println("Server: MouseControl Mode");
+							ois = new ObjectInputStream(client.getInputStream());
+							do {
+//								mouse = MouseInfo.getPointerInfo()
+//										.getLocation();
+								coordinate = (int[]) ois.readObject();
+								robot.mouseMove(4*coordinate[0],
+										(int) (1.35* coordinate[1]));
+							} while (j != 4);
+						}
 					}
+				} catch (Exception e) {
+					System.out.println("Server: Error");
+					e.printStackTrace();
 				}
 			}
 		});
 		Server.start();
 	}
+
 }
