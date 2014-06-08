d04a3ea47629c88bda4b484cb0dc32446971bd0e
Heiru Wu <wuheiru.5203@gmail.com>
2014-06-08 17:27:14 +0800

    Mouse Scrolling behave finished

--- src/RCServer/RCServer.java
+++ src/RCServer/RCServer.java
@@ -2,6 +2,7 @@ package RCServer;
 
 import java.awt.AWTException;
 import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
@@ -17,7 +18,7 @@ public class RCServer {
 	static InputStream in;
 	static ObjectInputStream ois;
 	static Socket client;
-	static int[] coordinate;
+	static Point mouse;
 
 	// static Point mouse;
 
@@ -27,6 +28,7 @@ public class RCServer {
 
 			@Override
 			public void run() {
+				int[] coordinate = new int[]{};
 				// TODO Auto-generated method stub
 				int ServerPort = 1025;
 				System.out.println("Server: Waiting for connection......");
@@ -94,23 +96,25 @@ public class RCServer {
 							System.out.println("Server: MouseControl Mode");
 							ois = new ObjectInputStream(client.getInputStream());
 							while (true) {
-								// mouse = MouseInfo.getPointerInfo()
-								// .getLocation();
 								coordinate = (int[]) ois.readObject();
-								if (coordinate[0] != 4) {
-									robot.mouseMove(4 * coordinate[0],
-											(int) (1.35 * coordinate[1]));
+								if (coordinate[0] != 2000) {
+									 mouse = MouseInfo.getPointerInfo()
+											 .getLocation();
+//									robot.mouseMove(4 * coordinate[0],
+//											(int) (1.35 * coordinate[1]));
+									robot.mouseMove(mouse.x-coordinate[0], mouse.y-coordinate[1]);
 									if(coordinate[0] == 1){
-										robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-										robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+//										robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//										robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 									}
 								} else {
 									System.out
 											.println("Server: MouseControl Mode Cancelled");
 									break;
 								}
-							}
-							;
+								coordinate[0] = 0;
+								coordinate[1] = 0;
+							};
 						}
 					}
 				} catch (Exception e) {
