993c9cac1a9fff04d062afd116690fdedc1ea537
gt212345 <wuheiru.5203@gmail.com>
2014-06-09 15:15:11 +0800

    Delete unused import

--- src/RCServer/RCServer.java
+++ src/RCServer/RCServer.java
@@ -9,7 +9,6 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
-import java.lang.reflect.Field;
 import java.net.ServerSocket;
 import java.net.Socket;
 
